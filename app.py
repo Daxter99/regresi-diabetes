4cb28c39567672350143ee66aba39b9d897ddd2d
Daxter99 <jajakjuni7@gmail.com>
2023-12-03 22:17:58 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import pickle
+import numpy as np
+import streamlit as st
+
+model = pickle.load(open('diabetes.sav', 'rb'))
+
+st.title('Klasifikasi Penyakit Diabetes Menggunakan Algoritma Regresi')
+
+col1, col2 = st.columns(2)
+
+#membuat form inputan
+with col1:  
+    Pregnancies = st.number_input('Input Nilai Pregnancies')
+with col2:
+    Glucose = st.number_input('Input Nilai Glucose')
+with col1:
+    BloodPressure = st.number_input('Input Nilai Blood Pressure')
+with col2:
+    SkinThickness = st.number_input('Input Nilai Skin Thickness')
+with col1:
+    Insulin = st.number_input('Input Nilai Insulin')
+with col2:
+    Bmi = st.number_input('Input Nilai BMI')
+with col1:
+    DiabetesPedigreeFunction = st.number_input('Input Nilai Diabetes Pedigree Function')
+with col2:
+    Age = st.number_input('Input Nilai Age')
+
+#coding prediksi
+input_data = ''
+
+#membuat button prediksi
+if st.button('Prediksi?'):
+    diab_prediction = model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, Bmi, DiabetesPedigreeFunction, Age]])
+    
+    if(diab_prediction [0] == 1):
+        input_data = 'Pasien terkena Diabetes'
+    else :
+        input_data = 'Pasien TIDAK terkena Diabetes'
+
+    st.success(input_data)
